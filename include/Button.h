db752e16a454f667641e5044f694b869f68d9090
david <david@david-Aspire-V5-121.(none)>
2013-11-22 17:35:20 +0100

    ajout des boutons / images

---
+++ include/Button.h
@@ -0,0 +1,29 @@
+#ifndef BUTTON_H
+#define BUTTON_H
+#include <iostream>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+class Button
+{
+    public:
+        Button(int x,int y,const char * path);
+        virtual ~Button();
+        SDL_Surface* GetbuttonSurface() { return buttonSurface_; }
+        void SetbuttonSurface(SDL_Surface* val) { buttonSurface_ = val; }
+        SDL_Rect GetbuttonPosition() { return buttonPosition_; }
+        void SetbuttonPosition(SDL_Rect val) { buttonPosition_ = val; }
+        void applyButton(SDL_Surface* surface);
+        void setPathImg(const char* path);
+        bool checkClick(int mouseX, int mouseY);
+        void freeButtonSurface();
+
+    protected:
+
+    private:
+        SDL_Surface* buttonSurface_;
+        SDL_Surface* buttonSurfaceOpt_;
+        SDL_Rect buttonPosition_;
+};
+
+#endif // BUTTON_H
---
+++ src/Button.cpp
@@ -0,0 +1,44 @@
+#include "../include/Button.h"
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+
+Button::Button(int x,int y,const char * path)
+{
+    buttonSurface_= IMG_Load(path);
+    buttonSurfaceOpt_=SDL_DisplayFormatAlpha(buttonSurface_);
+    buttonPosition_.x=x;
+    buttonPosition_.y=y;
+}
+
+
+void Button::applyButton(SDL_Surface* surface)
+{
+
+  SDL_BlitSurface(this->buttonSurfaceOpt_, NULL, surface, &this->buttonPosition_);
+
+}
+
+void Button::setPathImg(const char* path){
+    SDL_FreeSurface(this->buttonSurface_);
+    this->buttonSurface_=IMG_Load(path);
+    buttonSurfaceOpt_=SDL_DisplayFormatAlpha(this->buttonSurface_);
+}
+
+bool Button::checkClick(int mouseX, int mouseY)
+{
+    if(this->buttonPosition_.x < mouseX && mouseX < this->buttonPosition_.x + this->buttonSurface_->w &&
+    this->buttonPosition_.y< mouseY && mouseY < this->buttonPosition_.y + this->buttonSurface_->h)
+    {return true; }
+    else return false;
+}
+
+void Button::freeButtonSurface(){
+
+SDL_FreeSurface(this->buttonSurface_);
+}
+
+
+Button::~Button()
+{
+    //dtor
+}
